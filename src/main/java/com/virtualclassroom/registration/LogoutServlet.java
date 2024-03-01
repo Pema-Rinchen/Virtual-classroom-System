a1f4af08bdb0a624cca30586072354b3991bc39b
Pema Rinchen <111570488+Pema-Rinchen@users.noreply.github.com>
2024-03-01 22:51:08 +0530

    Update LogoutServlet.java

--- src/main/java/com/virtualclassroom/registration/LogoutServlet.java
+++ src/main/java/com/virtualclassroom/registration/LogoutServlet.java
@@ -19,7 +19,7 @@ public class LogoutServlet extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		HttpSession session=request.getSession();
 		session.invalidate();
-		response.sendRedirect("login.jsp");
+		response.sendRedirect("login.jsp")//Nothing to do here
 	}
 
 
